ae837999378754cac28edc73f711595ed7c65158
Anderbone <yanchiyu@outlook.com>
2023-12-06 16:04:29 +0000

    remove extra autoconfiguration

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,6 +1,5 @@
 package example.cashcard;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,7 +13,6 @@ import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
-@AutoConfiguration
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
