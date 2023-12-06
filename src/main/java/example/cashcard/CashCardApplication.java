8f9bdd7bbeee6064c4a39719c248efa8f8ad211a
Anderbone <yanchiyu@outlook.com>
2023-12-06 16:01:23 +0000

    WIP

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CashCardApplication {
-	
+
 	public static void main(String[] args) {
-		
 		SpringApplication.run(CashCardApplication.class, args);
 	}
-	
+
 }
